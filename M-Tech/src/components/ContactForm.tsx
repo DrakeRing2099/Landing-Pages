1ea70833e93c32b479a7ba4290334752abb02b35
Ramya-Shah <176913971+Ramya-Clg@users.noreply.github.com>
2025-04-04 01:11:58 +0530

    states

--- M-Tech/src/components/ContactForm.tsx
+++ M-Tech/src/components/ContactForm.tsx
@@ -202,19 +202,26 @@ const ContactForm = () => {
                                         onChange={handleInputChange}
                                         required
                                     >
-                                        <option value="">State</option>
+                                        <option value="">State/Union Territory</option>
+                                        <option>Andaman and Nicobar Islands</option>
                                         <option>Andhra Pradesh</option>
                                         <option>Arunachal Pradesh</option>
                                         <option>Assam</option>
                                         <option>Bihar</option>
+                                        <option>Chandigarh</option>
                                         <option>Chhattisgarh</option>
+                                        <option>Dadra and Nagar Haveli and Daman and Diu</option>
+                                        <option>Delhi</option>
                                         <option>Goa</option>
                                         <option>Gujarat</option>
                                         <option>Haryana</option>
                                         <option>Himachal Pradesh</option>
+                                        <option>Jammu and Kashmir</option>
                                         <option>Jharkhand</option>
                                         <option>Karnataka</option>
                                         <option>Kerala</option>
+                                        <option>Ladakh</option>
+                                        <option>Lakshadweep</option>
                                         <option>Madhya Pradesh</option>
                                         <option>Maharashtra</option>
                                         <option>Manipur</option>
@@ -222,6 +229,7 @@ const ContactForm = () => {
                                         <option>Mizoram</option>
                                         <option>Nagaland</option>
                                         <option>Odisha</option>
+                                        <option>Puducherry</option>
                                         <option>Punjab</option>
                                         <option>Rajasthan</option>
                                         <option>Sikkim</option>
@@ -231,7 +239,6 @@ const ContactForm = () => {
                                         <option>Uttar Pradesh</option>
                                         <option>Uttarakhand</option>
                                         <option>West Bengal</option>
-
                                     </select>
                                 </div>
 
